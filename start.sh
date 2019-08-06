015184a5f2c5b85cebfb05d7f2474c661d42228f
Carlos Bustamante <cbustamante@consultoresinformaticos.com.py>
2019-08-06 16:51:53 -0400

    release with network

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -e POSTGRES_USER=docker -e POSTGRES_PASSWORD=docker -e POSTGRES_DB=db_academy --rm -d -h testdb --dns-opt='options single-request' --sysctl net.ipv6.conf.all.disable_ipv6=1  -p 15432:5432 --network=bridge --name=testdb oraclegbpy/test_postgres 
+docker run -e POSTGRES_USER=docker -e POSTGRES_PASSWORD=docker -e POSTGRES_DB=db_academy --rm -d -h testdb  --network=br0 -p 15432:5432  --name=testdb --ip 10.10.10.4 oraclegbpy/test_postgres 
---
+++ testdb.sh
@@ -0,0 +1,1 @@
+	docker run -it --rm --network=br0 jbergknoff/postgresql-client postgresql://docker:docker@testdb:5432/db_academy
